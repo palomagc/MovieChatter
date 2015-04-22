e483880c6d2d5c20c69256f6e915dcacf180d575
AdrianVader <adri.rabadan@gmail.com>
2015-04-22 03:20:42 +0200

    Agentes DialogoCitas y Guia comunicandose

--- src/icaro/aplicaciones/informacion/gestionCitas/NotificacionInterlocutor.java
+++ src/icaro/aplicaciones/informacion/gestionCitas/NotificacionInterlocutor.java
@@ -19,4 +19,44 @@ public class NotificacionInterlocutor {
 		tipoNotificacion = notif.getTipoNotificacion();
 	}
 	
+	public String getIdentNotificador() {
+		return identNotificador;
+	}
+
+	public void setIdentNotificador(String identNotificador) {
+		this.identNotificador = identNotificador;
+	}
+
+	public String getMensajeNotificacion() {
+		return mensajeNotificacion;
+	}
+
+	public void setMensajeNotificacion(String mensajeNotificacion) {
+		this.mensajeNotificacion = mensajeNotificacion;
+	}
+
+	public Object getContexto() {
+		return contexto;
+	}
+
+	public void setContexto(Object contexto) {
+		this.contexto = contexto;
+	}
+
+	public String getIdentObjectRefNotificacion() {
+		return identObjectRefNotificacion;
+	}
+
+	public void setIdentObjectRefNotificacion(String identObjectRefNotificacion) {
+		this.identObjectRefNotificacion = identObjectRefNotificacion;
+	}
+
+	public String getTipoNotificacion() {
+		return tipoNotificacion;
+	}
+
+	public void setTipoNotificacion(String tipoNotificacion) {
+		this.tipoNotificacion = tipoNotificacion;
+	}
+	
 }
