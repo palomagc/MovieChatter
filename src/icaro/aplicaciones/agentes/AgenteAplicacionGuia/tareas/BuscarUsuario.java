9c7c61f2e95e6988e21c1050d7ae48435616e470
AdrianVader <adri.rabadan@gmail.com>
2015-05-08 02:30:59 +0200

    algunas reglas del agente guia añadidas falta ponerle tareas
    algunas reglas del agente guia añadidas falta ponerle tareas

--- src/icaro/aplicaciones/agentes/AgenteAplicacionGuia/tareas/BuscarUsuario.java
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionGuia/tareas/BuscarUsuario.java
@@ -1,5 +1,7 @@
 package icaro.aplicaciones.agentes.AgenteAplicacionGuia.tareas;
 
+import icaro.aplicaciones.agentes.AgenteAplicacionGuia.objetivos.PreguntarDatosInicialesUsuario;
+import icaro.aplicaciones.agentes.AgenteAplicacionGuia.objetivos.RecomendarPelicula;
 import icaro.aplicaciones.informacion.gestionCitas.VocabularioGestionCitas;
 import icaro.aplicaciones.recursos.comunicacionChat.ItfUsoComunicacionChat;
 import icaro.infraestructura.entidadesBasicas.NombresPredefinidos;
@@ -28,14 +30,18 @@ public class BuscarUsuario extends TareaSincrona {
 		String identAgenteOrdenante = this.getIdentAgente();
 		String identInterlocutor = (String) params[0];
 
-		// identInterlocutor contiene el nombre del usuario, iniciar la base de
-		// datos y buscar al usuario
-		// Si no está, lo creas nuevo y se le responderá como nuevo usuario. Se
-		// le intentará preguntar por sus datos básicos
-		// Si está, coges todos sus datos y le dices que ya le conoces. Le
-		// preguntas por las ultimas pelis que le pusiste y por datos básicos
-		// que falten
-
+		// identInterlocutor contiene el nombre del usuario, iniciar la base de datos y buscar al usuario
+		boolean exists = false;
+		// Si no está, lo creas nuevo y se le responderá como nuevo usuario. Se le intentará preguntar por sus datos básicos
+		if(!exists){
+			this.getEnvioHechos().insertarHecho(new PreguntarDatosInicialesUsuario());
+		}
+		// Si está, coges todos sus datos y le dices que ya le conoces. Le preguntas por las ultimas pelis que le pusiste y por datos básicos que falten
+		else{
+			// AQUI PUEDE QUE TE DES CUENTA DE QUE VIO UNA PELI Y QUERRAS PREGUNTARLE SI QUIERE VALORARLA (HAY QUE LANZAR EL HECHO Y METER LA REGLA EN EL AGENTE GUIA)
+			this.getEnvioHechos().insertarHecho(new RecomendarPelicula());
+		}
+		
 		try {
 			// // Se busca la interfaz del recurso en el repositorio de
 			// interfaces
