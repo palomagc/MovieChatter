12c984801e67f4c2cee6d62815c19f1f3efa52d0
liith <teresuky7@gmail.com>
2015-05-12 01:22:55 +0200

    Resuelto fallo: no añadía el usuario a la base de datos

--- src/icaro/aplicaciones/recursos/recursoUsuario/ClaseGeneradoraRecursoUsuario.java
+++ src/icaro/aplicaciones/recursos/recursoUsuario/ClaseGeneradoraRecursoUsuario.java
@@ -28,14 +28,16 @@ public class ClaseGeneradoraRecursoUsuario extends ImplRecursoSimple implements 
 
 	@Override
 	public Usuario crearUsuario(String nombre, String sexo, int edad) {
-		return new Usuario(nombre, sexo, edad);
-
+		Usuario usuario = new Usuario(nombre, sexo, edad);
+		usuario.addUsuarioBD();
+		return usuario;
 	}
 
 	@Override
 	public Usuario crearUsuario(String nombre, String sexo, int edad, ArrayList<Valoracion> valoraciones, ArrayList<String> generosPreferidos, ArrayList<String> actoresPreferidos, ArrayList<String> actoresOdiados) {
-		return new Usuario(nombre, sexo, edad, valoraciones, generosPreferidos, actoresPreferidos, actoresOdiados);
-
+		Usuario usuario = new Usuario(nombre, sexo, edad, valoraciones, generosPreferidos, actoresPreferidos, actoresOdiados);
+		usuario.addUsuarioBD();
+		return usuario;
 	}
 
 	@Override
